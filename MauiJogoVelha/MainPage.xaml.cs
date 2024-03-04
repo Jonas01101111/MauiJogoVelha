90af48f8204f8c6427789830929b18da5da080b9
Usuario <Usuario@CPD03-M06>
2024-03-04 14:59:49 -0300

    codigo

--- MauiJogoVelha/MainPage.xaml.cs
+++ MauiJogoVelha/MainPage.xaml.cs
@@ -2,11 +2,29 @@
 {
     public partial class MainPage : ContentPage
     {
+        string vez = "O";
         public MainPage()
         {
             InitializeComponent();
         }
 
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Button disparador = (Button)sender;
+            disparador.IsEnabled = false;
+
+            if (vez == "O")
+            {
+                disparador.Text = "O";
+                vez = "X";
+            }
+            else
+            {
+                disparador.Text = "X";
+                vez = "O";
+            }
+            
+        }//fim btn clicked
     }
 
 }
